5653b6b2ae9fd79b4471bcbd8d4b36c4bbebddaa
Nofangagah <zohrialpoison@gmail.com>
2024-11-18 01:17:34 +0700

    add register and login

--- firebaseClient.js
+++ firebaseClient.js
@@ -2,8 +2,8 @@ const Firestore = require("@google-cloud/firestore");
 require("dotenv").config();
 
 const db = new Firestore({
-    projectId: "capstone-c242-ps555",
-    keyFilename: "./service-account.json",
+    projectId: process.env.PROJECT_ID,
+    keyFilename:  process.env.GOOGLE_APPLICATION_CREDENTIALS,
     databaseId : process.env.DATABASE_ID
 });
 
